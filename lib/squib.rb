e37ca450c16f26602ecbb351ca891f2fb846109e
Andy Meneely <andy@se.rit.edu>
2014-07-08 22:10:35 -0400

    Sketching out a visitor pattern

--- lib/squib.rb
+++ lib/squib.rb
@@ -1,17 +1,21 @@
-require "squib/version"
-
 module Squib
+
 end
 
 ##################
 ### PUBLIC API ###
 ##################
 
+def deck(width:, height:, cards: 1)
+
+end
+
 def font(type: , size: 12, **options)
+
 end
 
 def set_font(type: 'Arial', size: 12, **options)
-
+	Squib::queue_command Squib::Commands::SetFont.new(type,size,options)
 end
 
 def text(range=:all, str: , font: :use_set, x: 0, y: 0, **options)
@@ -24,4 +28,8 @@ def load_csv(file:, header: true)
 end
 
 def data(field)
+end
+
+def render
+	Squib::Verify::VerifyAll.
 end
