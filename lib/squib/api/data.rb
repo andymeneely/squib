f778b2f9204bd17f708d14d0a902fd4b0e4896e7
Andy Meneely <andy@se.rit.edu>
2014-07-16 12:02:30 -0400

    Moving samples out of bin

--- lib/squib/api/data.rb
+++ lib/squib/api/data.rb
@@ -1,7 +1,14 @@
+require 'spreadsheet'
+
 module Squib
   class Deck
+
     def csv(file: 'deck.csv', header: true)
       raise 'Not implemented!'
+    end
+
+    def xls(file: 'data.xls', sheet: 0)
+      
     end
   end
 end
