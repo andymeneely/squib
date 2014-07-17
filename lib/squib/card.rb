d7c8941620def9fb0ec9098ae20577edfc25a277
Andy Meneely <andy@se.rit.edu>
2014-07-17 10:53:23 -0400

    Whitespace cleanup

--- lib/squib/card.rb
+++ lib/squib/card.rb
@@ -1,27 +1,25 @@
 require 'cairo'
 
 module Squib
-
 	class Card
-    attr_reader :width, :height
-    attr_accessor :cairo_surface, :cairo_context
+        attr_reader :width, :height
+        attr_accessor :cairo_surface, :cairo_context
 
-    def initialize(width, height)
-      @width=width; @height=height
-      @cairo_surface = Cairo::ImageSurface.new(width,height)
-      @cairo_context = Cairo::Context.new(@cairo_surface)
-    end
+        def initialize(width, height)
+          @width=width; @height=height
+          @cairo_surface = Cairo::ImageSurface.new(width,height)
+          @cairo_context = Cairo::Context.new(@cairo_surface)
+        end
 
-    ########################
-    ### BACKEND GRAPHICS ###
-    ########################
-    require 'squib/graphics/background'
-    require 'squib/graphics/image'
-    require 'squib/graphics/save_doc'
-    require 'squib/graphics/save_images'
-    require 'squib/graphics/shapes'
-    require 'squib/graphics/text'
+        ########################
+        ### BACKEND GRAPHICS ###
+        ########################
+        require 'squib/graphics/background'
+        require 'squib/graphics/image'
+        require 'squib/graphics/save_doc'
+        require 'squib/graphics/save_images'
+        require 'squib/graphics/shapes'
+        require 'squib/graphics/text'
     
 	end
-
 end
