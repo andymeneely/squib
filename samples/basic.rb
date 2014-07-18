b0bf7f65bd66524513ff62bd5b41af9b49608370
Andy Meneely <andy@se.rit.edu>
2014-07-18 15:52:56 -0400

    Cleaning up some samples

--- samples/basic.rb
+++ samples/basic.rb
@@ -6,14 +6,15 @@ data = {'name' => ['Thief', 'Grifter', 'Mastermind'],
 
 Squib::Deck.new(width: 825, height: 1125, cards: 3) do
   background color: :white
-  rect x: 15, y: 15, width: 795, height: 1095, x_radius: 50, y_radius: 50
-  rect x: 30, y: 30, width: 128, height: 128, x_radius: 25, y_radius: 25
+  rect x: 38, y: 38, width: 750, height: 1050, x_radius: 38, y_radius: 38
+  rect x: 75, y: 75, width: 128, height: 128, x_radius: 25, y_radius: 25
 
-  text str: data['name'], x: 250, y: 55, font: 'Arial 54'
-  text str: data['level'], x: 65, y: 40, font: 'Arial 72'
+  text str: data['name'], x: 220, y: 78, font: 'Arial 54'
+  text str: data['level'], x: 75, y: 85, width: 128, 
+       font: 'Arial 72', align: :center
 
-  png range: [0,2], file: 'shiny-purse.png', x: 665, y: 30
-  svg range: 1..2, file: 'spanner.svg', x: 665, y: 165
+  png range: [0,2], file: 'shiny-purse.png', x: 620, y: 75
+  svg range: 1..2, file: 'spanner.svg', x: 620, y: 218
 
   save format: :png
 end
--- samples/ranges.rb
+++ samples/ranges.rb
@@ -8,8 +8,6 @@ data = {'name' => ['Thief', 'Grifter', 'Mastermind'],
 Squib::Deck.new(width: 825, height: 1125, cards: 3) do
   # Default range is :all
   background color: :white
-  rect x: 15, y: 15, width: 795, height: 1095, x_radius: 50, y_radius: 50
-  rect x: 30, y: 30, width: 128, height: 128, x_radius: 25, y_radius: 25
   text str: data['name'], x: 250, y: 55, font: 'Arial 54'
   text str: data['level'], x: 65, y: 40, font: 'Arial 72'
 
