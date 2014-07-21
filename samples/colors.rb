209b4240eb3646e2ed2e763fb00cfd3afd2b0e4c
Andy Meneely <andy@se.rit.edu>
2014-07-21 17:02:25 -0400

    Added a colors example

---
+++ samples/colors.rb
@@ -0,0 +1,17 @@
+require 'squib'
+
+Squib::Deck.new(width: 825, height: 1125, cards: 1) do
+  background color: :white
+
+  y = 0
+  text color: '#f00', str: '3-hex', x: 50, y: y+=50
+  text color: '#f00', str: '3-hex (alpha)', x: 50, y: y+=50
+  text color: '#ff0000', str: '6-hex', x: 50, y: y+=50
+  text color: '#ff000099', str: '8-hex(alpha) *', x: 50, y: y+=50
+  text color: '#ffff00000000', str: '12-hex', x: 50, y: y+=50
+  text color: '#ffff000000009999', str: '12-hex (alpha)', x: 50, y: y+=50
+  text color: :burnt_orange, str: 'Symbols of constants too', x: 50, y: y+=50
+  text color: [1.0, 0.0, 0.0], str: 'Array of percentages', x: 50, y: y+=50
+
+  save_png prefix: "colors_"
+end
