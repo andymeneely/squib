a59a3a5790b375e97a19e9b9d636c3d0af89da14
Andy Meneely <andy@se.rit.edu>
2014-07-23 09:34:50 -0400

    Removing debug statement

--- spec/samples_run_spec.rb
+++ spec/samples_run_spec.rb
@@ -5,7 +5,6 @@ describe Squib do
   it "should execute all examples with no errors" do
 
     samples = File.expand_path('../samples', File.dirname(__FILE__))
-    puts "Samples: #{samples}"
     Dir.chdir(samples) do
       require_relative '../samples/basic.rb'
     end
