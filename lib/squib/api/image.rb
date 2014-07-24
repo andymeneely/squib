d2e99d5bf6213f0904f776e477f675d8bd82a405
Andy Meneely <andy@se.rit.edu>
2014-07-23 21:47:05 -0400

    Actually documenting said svg id

--- lib/squib/api/image.rb
+++ lib/squib/api/image.rb
@@ -21,7 +21,8 @@ module Squib
     #   See {file:samples/load-images.rb samples/load-images.rb} and {file:samples/tgc-overlay.rb samples/tgc-overlay.rb} as examples.
     #
     # @param range: the range of cards over which this will be rendered. See {file:API.md#label-Specifying+Ranges Specifying Ranges}
-    # @param file: the . See {file:API.md#Specifying+Files Specifying Files}
+    # @param file: the svg file to render. See {file:API.md#Specifying+Files Specifying Files}
+    # @param id: if specified, only render the SVG element with the supplied id. Otherwise, render the entire SVG file
     # @param x: the x-coordinate to place
     # @param y: the y-coordinate to place
     # @param width: the pixel width that the image should scale to. SVG scaling is done with vectors, so the scaling should be smooth. When set to `:native`, uses the DPI and units of the loaded SVG document.
