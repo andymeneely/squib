5e8fa54d9244eb9062166823d4798e28e08de795
Andy Meneely <andy@se.rit.edu>
2014-07-23 23:11:34 -0400

    Updating spec to run all samples

--- spec/samples_run_spec.rb
+++ spec/samples_run_spec.rb
@@ -3,10 +3,11 @@ require 'squib'
 describe Squib do 
 
   it "should execute all examples with no errors" do
-
     samples = File.expand_path('../samples', File.dirname(__FILE__))
-    Dir.chdir(samples) do
-      require_relative '../samples/basic.rb'
+    Dir["#{samples}/**/*.rb"].each do |sample|
+      Dir.chdir(samples) do #to save to _output
+        require_relative "../samples/#{File.basename(sample)}"
+      end
     end
   end
 
