70f8eb3643ba5966b4048c35c36352fa3d4da32a
Andy Meneely <andy@se.rit.edu>
2014-07-23 22:39:56 -0400

    Adding more to the sample

--- samples/text_options.rb
+++ samples/text_options.rb
@@ -10,8 +10,11 @@ Squib::Deck.new(width: 825, height: 1125, cards: 3) do
   rect x: 15, y: 15, width: 795, height: 1095, x_radius: 50, y_radius: 50
   rect x: 30, y: 30, width: 128, height: 128, x_radius: 25, y_radius: 25
 
+  # Arrays are rendered over each card
   text str: data['name'], x: 250, y: 55, font: 'Arial weight=900 54'
   text str: data['level'], x: 65, y: 40, font: 'Arial 72', color: :burnt_orange
+
+  text str: "UTF-8 \u2663", x: 565, y: 150, font: 'Arial 54 weight=900 36'
 
   text str: "Font strings are expressive!", x:65, y: 200,
        font: 'Impact bold italic 36'
