fddb82668da8a5055238553dcde29b67743e2d18
Andy Meneely <andy@se.rit.edu>
2014-10-17 08:43:44 -0400

    Version bump

--- lib/squib/version.rb
+++ lib/squib/version.rb
@@ -1,6 +1,10 @@
 module Squib
 
   # The next version to be released.
-  # Uses semantic versioning
-  VERSION = "0.0.4"
+  # Uses semantic versioning: http://semver.org/
+  #
+  # Most of the time this is in the alpha of the next release. 
+  # e.g. v0.0.5a is on its way to becoming v0.0.5
+  # 
+  VERSION = "0.0.5a"
 end
