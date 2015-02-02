1896689614ef64a21e1e6e060ddbbbf2f8165699
Andy Meneely <andy@se.rit.edu>
2015-02-01 23:48:09 -0500

    Refactoring out the unit conversion to its own place

--- lib/squib/input_helpers.rb
+++ lib/squib/input_helpers.rb
@@ -1,4 +1,5 @@
 require 'squib/constants'
+require 'squib/args/unit_conversion'
 
 module Squib
   # :nodoc:
@@ -200,12 +201,7 @@ module Squib
       Squib::UNIT_CONVERSION_PARAMS.each_pair do |param_name, api_param|
         if needed_params.include? param_name
           opts[api_param].each_with_index do |arg, i|
-            case arg.to_s.rstrip
-            when /in$/ #ends with "in"
-              opts[api_param][i] = arg.rstrip[0..-2].to_f * @dpi
-            when /cm$/ #ends with "cm"
-              opts[api_param][i] = arg.rstrip[0..-2].to_f * @dpi * Squib::INCHES_IN_CM
-            end
+            opts[api_param][i] = Args::UnitConversion.parse(arg, @dpi)
           end
         end
       end
