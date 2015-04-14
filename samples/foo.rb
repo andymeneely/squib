838b1fd08503ff3293a7262aef3d3d136d7d4132
Andy Meneely <andy@se.rit.edu>
2015-04-13 23:19:59 -0400

    Removing scratch code

--- samples/foo.rb
+++
@@ -1,8 +0,0 @@
-require 'squib'
-puts "Using Squib #{Squib::VERSION}"
-
-Squib::Deck.new(cards: 3, dpi: 600) do
-  background color: :gray
-  text str: %w(Hello fine world!)
-  save_pdf file: 'foo.pdf', gap: 5, width: '11in', height: '8.5in'
-end
