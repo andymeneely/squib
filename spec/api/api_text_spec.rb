b03dbb54da80da66fe18c31d5a74d7b6ff0df3e2
Andy Meneely <andy@se.rit.edu>
2015-04-28 22:31:15 -0400

    Fix unit test breakage

--- spec/api/api_text_spec.rb
+++ spec/api/api_text_spec.rb
@@ -6,7 +6,7 @@ describe Squib::Deck, '#text' do
   context 'fonts' do
      it "should use the default font when #text and #set_font don't specify" do
       card = instance_double(Squib::Card)
-      expect(card).to receive(:text).with(anything, 'a', 'Arial 36', *([anything] * 15)).once
+      expect(card).to receive(:text).with(anything, 'a', 'Arial 36', *([anything] * 17)).once
       Squib::Deck.new do
         @cards = [card]
         text str: 'a'
@@ -15,7 +15,7 @@ describe Squib::Deck, '#text' do
 
     it "should use the #set_font when #text doesn't specify" do
       card = instance_double(Squib::Card)
-      expect(card).to receive(:text).with(anything, 'a', 'Times New Roman 16', *([anything] * 15)).once
+      expect(card).to receive(:text).with(anything, 'a', 'Times New Roman 16', *([anything] * 17)).once
       Squib::Deck.new do
         @cards = [card]
         set font: 'Times New Roman 16'
@@ -25,7 +25,7 @@ describe Squib::Deck, '#text' do
 
     it 'should use the specified font no matter what' do
       card = instance_double(Squib::Card)
-      expect(card).to receive(:text).with(anything, 'a', 'Arial 18', *([anything] * 15)).once
+      expect(card).to receive(:text).with(anything, 'a', 'Arial 18', *([anything] * 17)).once
       Squib::Deck.new do
         @cards = [card]
         set font: 'Times New Roman 16'
