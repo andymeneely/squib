ac7769237c04122aedef107817591f124a5318f8
Andy Meneely <andy@se.rit.edu>
2015-04-30 10:05:34 -0400

    Fix the csv_import.rb sample to function properly

--- samples/csv_import.rb
+++ samples/csv_import.rb
@@ -5,15 +5,13 @@ Squib::Deck.new(cards: 2) do
 
   # Outputs a hash of arrays with the header names as keys
   data = csv file: 'sample.csv'
-
-  text str: data['Name'], x: 250, y: 55, font: 'Arial 54'
+  text str: data['Type'], x: 250, y: 55, font: 'Arial 54'
   text str: data['Level'], x: 65, y: 65, font: 'Arial 72'
-  text str: data['Description'], x: 65, y: 600, font: 'Arial 36'
+
+  save format: :png, prefix: 'sample_csv_'
 
   # You can also specify the sheet, starting at 0
   data = xlsx file: 'sample.xlsx', sheet: 2
-
-  save format: :png, prefix: 'sample_csv_'
 end
 
 # CSV is also a Squib-module-level function, so this also works:
