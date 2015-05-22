c3e76a93aaf0961d4bb44d620c83fc86f6ca3f2c
Andy Meneely <andy.meneely@gmail.com>
2015-05-22 05:36:32 -0400

    Fix a syntax error

--- lib/squib/constants.rb
+++ lib/squib/constants.rb
@@ -149,7 +149,7 @@ module Squib
       :dy => :dx, # delta
       :gap => :gap,
       :height => :height,
-      :inner_radius => :inner_radius
+      :inner_radius => :inner_radius,
       :margin => :margin,
       :outer_radius => :outer_radius,
       :paper_width => :width,
