42aa269789ea98c8401b3edf18eb26914ff34964
andrew <azarkon@gmail.com>
2016-01-07 16:39:19 -0500

    Refine factory methods and Deck::new to account for bleed argument

--- lib/squib/deck.rb
+++ lib/squib/deck.rb
@@ -10,7 +10,6 @@ require_relative 'graphics/showcase'
 require_relative 'layout_parser'
 require_relative 'progress'
 
-require 'byebug'
 
 # The project module
 #
@@ -53,14 +52,14 @@ module Squib
     #
     # @param width [Integer] the width of each card in pixels. Supports unit conversion (e.g. '2.5in').
     # @param height [Integer] the height of each card in pixels. Supports unit conversion (e.g. '3.5in').
+    # @param bleed [Integer] the size of the bleed in pixels. Supports unit conversion(e.g. '0.125in').  Height and width should specify the final size of the card after trimming, the actual size of the card will be extended by the bleed on all sides.  Thus, a card specified with a width of 2.5in, a height of 3.5in, and a bleed of 0.125in will have a total size of 2.75in by 3.75in. 
     # @param cards [Integer] the number of cards in the deck.
     # @param dpi [Integer] the pixels per inch when rendering out to PDF or calculating using inches.
-    # @param bleed [Integer] the size of the bleed in pixels. Supports unit conversion(e.g. '0.125in').
     # @param config [String] the file used for global settings of this deck.
     # @param layout [String, Array] load a YML file of custom layouts. Multiple files are merged sequentially, redefining collisons. See README and sample for details.
     # @param block [Block] the main body of the script.
     # @api public
-    def initialize(width: 825, height: 1125, cards: 1, dpi: 300, bleed: 0, config: 'config.yml', layout: nil,  &block)
+    def initialize(width: 825, height: 1125, cards: 1, dpi: 300, bleed: 0, config: 'config.yml', layout: nil, **factory_args,  &block)
       @bleed       = Args::UnitConversion.parse bleed, dpi 
       @dpi           = dpi
       @font          = DEFAULT_FONT
@@ -68,38 +67,42 @@ module Squib
       @conf          = Conf.load(config)
       @progress_bar  = Progress.new(@conf.progress_bars) # FIXME this is evil. Using something different with @ and non-@
       show_info(config, layout)
-      @width         = Args::UnitConversion.parse width, dpi
-      @height        = Args::UnitConversion.parse height, dpi
+      @width         = Args::UnitConversion.parse(width, dpi) + @bleed * 2
+      @height        = Args::UnitConversion.parse(height, dpi) + @bleed * 2
       cards.times{ |i| @cards << Squib::Card.new(self, @width, @height, i) }
       @layout        = LayoutParser.load_layout(layout)
+      @card_name = factory_args[:card_name]
+      @vendor = factory_args[:vendor]
       if block_given?
         instance_eval(&block) # here we go. wheeeee!
       end
     end
 
     def self.method_missing(card, args = nil, &block)
-      if args.is_a?(Hash)
-        vendor = args.keys.include?(:vendor) ? args[:vendor] : nil
-        if vendor
-          vendor_opts = YAML.load_file("#{File.dirname(__FILE__)}/vendor/#{vendor}.yml")
-          if  vendor_opts['items'].include? card.to_s
-            vendor_args = vendor_opts['specs'][card.to_s] || vendor_opts['specs']['default']
+      if args
+        if args.is_a?(Hash) 
+          vendor = args.keys.include?(:vendor) ? args[:vendor] : nil
+          # Add error handling if invalid params.
+          if vendor
+            vendor_opts = YAML.load_file("#{File.dirname(__FILE__)}/vendor/#{vendor}.yml")
+            if  vendor_opts['items'].include? card.to_s
+              vendor_args = vendor_opts['specs'] || vendor_opts['specs']['default']
+            end
           end
+          base_opts = YAML.load_file("#{File.dirname(__FILE__)}/vendor/base.yml")
+          base_args = base_opts[card.to_s]
+          # Validate card is valid
+          args.merge!({ card_name: card })
+          args.merge! base_args if base_args
+          args.merge! vendor_args if vendor_args
+          sym_args = {}
+          args.each_pair do |key, val|
+            sym_args[key.to_sym] = val
+          end
+          Squib::Deck.new **sym_args, &block
         end
-        base_opts = YAML.load_file("#{File.dirname(__FILE__)}/vendor/base.yml")
-        base_args = base_opts[card.to_s]
-        # Validate card is valid
-        args.delete :vendor
-        args.merge! base_args if base_args
-        args.merge! vendor_args if vendor_args
-        # add logic to add blead to height and width
-        sym_args = {}
-        args.each_pair do |key, val|
-          sym_args[key.to_sym] = val
-        end
-        Squib::Deck.new **sym_args, &block
       else
-        super
+        super # run if errors are caught 
       end
     end
 
