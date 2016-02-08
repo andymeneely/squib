e7167b906d5d1f48c009f72eea437b56992e5468
Andy Meneely <andy@se.rit.edu>
2016-02-07 21:19:49 -0500

    docs: inline syntax highlighting

--- docs/conf.py
+++ docs/conf.py
@@ -284,3 +284,7 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
+
+# sphinx-contrib configurations
+inline_highlight_respect_highlight = True
+inline_highlight_literals = True
