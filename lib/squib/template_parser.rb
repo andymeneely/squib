e2749dfc0e73b00cbbf6e691179c587e13c1ded6
Seh Hui, Leong <felixleong@gmail.com>
2017-04-16 23:17:14 +0800

    Added iterators for crop lines and cards.

--- lib/squib/template_parser.rb
+++ lib/squib/template_parser.rb
@@ -5,22 +5,20 @@ require_relative 'args/unit_conversion'
 
 module Squib
   class Template
-    CROP_LINE_DEFAULTS = {
-      'style' => :solid,
-      'width' => '0.02mm',
-      'color' => :color,
-      'page_style' => :margin_only,
-      'lines' => []
-    }
-
-    # Defaults are set for poker sized deck on a A4 sheet
+    # Defaults are set for poker sized deck on a A4 sheet, with no cards
     DEFAULTS = {
       'sheet_width' => '210mm',
       'sheet_height' => '297mm',
       'card_width' => '63mm',
       'card_height' => '88mm',
       'dpi' => 300,
-      'crop_line' => CROP_LINE_DEFAULTS,
+      'crop_line' => {
+        'style' => :solid,
+        'width' => '0.02mm',
+        'color' => :color,
+        'page_style' => :margin_only,
+        'lines' => []
+      },
       'cards' => []
     }
 
@@ -28,6 +26,8 @@ module Squib
     def initialize(template_hash = DEFAULTS)
       ClassyHash.validate(template_hash, SCHEMA)
       @template_hash = template_hash
+      @crop_line_default = @template_hash.select {
+        |k, v| ["style", "width", "color"].include? k}
     end
 
     # Load the template definition file
@@ -61,8 +61,28 @@ module Squib
         @template_hash['card_height'], @template_hash['dpi'])
     end
 
+    def crop_lines
+      lines = @template_hash['crop_line']['lines'].map(
+        &method(:parse_crop_line))
+      if block_given?
+        lines.each { |v| yield v }
+      else
+        lines
+      end
+    end
+
+    def cards
+      cards = @template_hash['cards'].map(&method(:parse_card))
+      if block_given?
+        cards.each { |v| yield v }
+      else
+        cards
+      end
+    end
+
     private
 
+    # Template file schema
     UNIT_REGEX = /^(\d*[.])?\d+(in|cm|mm)?$/
     SCHEMA = {
       "sheet_width" => UNIT_REGEX,
@@ -99,6 +119,24 @@ module Squib
         Squib::logger.warn(
           "Unrecognized configuration option(s): #{unrec.join(',')}")
       end
+    end
+
+    # Parse crop line definitions from template.
+    def parse_crop_line(line)
+      new_line = @crop_line_default.merge line
+      new_line['width'] = Args::UnitConversion.parse(
+        new_line['width'], @template_hash['dpi'])
+      new_line
+    end
+
+    # Parse card definitions from template.
+    def parse_card(card)
+      new_card = card.rehash
+      new_card["x"] = Args::UnitConversion.parse(
+        card["x"], @template_hash["dpi"])
+      new_card["y"] = Args::UnitConversion.parse(
+        card["y"], @template_hash["dpi"])
+      new_card
     end
   end
 end
