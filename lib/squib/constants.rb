f141c9773a12d49b1e9ac92baf64a153a82ede01
Michael Zemel <michael.zemel@stitchfix.com>
2020-02-02 11:07:43 -0800

    Update comment to point to documentation

--- lib/squib/constants.rb
+++ lib/squib/constants.rb
@@ -15,7 +15,8 @@ module Squib
     # @api private
     POINTS_PER_IN = 72.0
 
-    # TODO: Add comment or link to Wiki article
+    # Used for coordinate-based layout
+    # https://github.com/andymeneely/squib/pull/295
     # :nodoc:
     # @api private
     DEFAULT_CELL_SIZE = 37.5
