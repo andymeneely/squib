f4543d220387efab9c295ba37f62e51de07b1d51
Andy Meneely <andy@se.rit.edu>
2021-10-20 23:51:12 -0400

    Initial work on rotate for save_sheet

--- lib/squib/graphics/save_doc.rb
+++ lib/squib/graphics/save_doc.rb
@@ -4,13 +4,12 @@ module Squib
     # :nodoc:
     # @api private
     def render_sheet(range, batch, sheet)
-      sheet_width = (sheet.columns * (@width + 2 * sheet.gap - 2 * sheet.trim)) + (2 * sheet.margin)
-      sheet_height = (sheet.rows * (@height + 2 * sheet.gap - 2 * sheet.trim)) + (2 * sheet.margin)
+      w,h,sheet_width,sheet_height = compute_dimensions(sheet, batch)
       cc = Cairo::Context.new(Cairo::ImageSurface.new(sheet_width, sheet_height))
       num_this_sheet = 0
       sheet_num = 0
       y = sheet.margin
-      x = sheet.rtl ? (sheet_width - sheet.margin - sheet.gap - @width) : sheet.margin
+      x = sheet.rtl ? (sheet_width - sheet.margin - sheet.gap - w) : sheet.margin
       @progress_bar.start("Saving PNG sheet to #{batch.summary}", @cards.size + 1) do |bar|
         range.each do |i|
           if num_this_sheet >= (sheet.columns * sheet.rows) # new sheet
@@ -20,25 +19,36 @@ module Squib
             num_this_sheet = 0
             sheet_num += 1
             y = sheet.margin
-            x = sheet.rtl ? (sheet_width - sheet.margin - sheet.gap - @width) : sheet.margin
+            x = sheet.rtl ? (sheet_width - sheet.margin - sheet.gap - w) : sheet.margin
             cc = Cairo::Context.new(Cairo::ImageSurface.new(sheet_width, sheet_height))
           end
-          surface = trim(@cards[i].cairo_surface, sheet.trim, @width, @height)
+          surface = preprocess(@cards[i].cairo_surface,
+                                sheet.trim, w, h,
+                                batch.rotate[i], batch.angle[i])
           cc.set_source(surface, x, y)
           cc.paint
           num_this_sheet += 1
           x += (surface.width + sheet.gap) * (sheet.rtl ? -1 : 1)
           if num_this_sheet % sheet.columns == 0 # new row
-            x = sheet.rtl ? (sheet_width - sheet.margin - sheet.gap - @width) : sheet.margin
+            x = sheet.rtl ? (sheet_width - sheet.margin - sheet.gap - w) : sheet.margin
             y += surface.height + sheet.gap
           end
+
           bar.increment
         end
         cc.target.write_to_png(batch.full_filename(sheet_num))
       end
     end
 
-    # Return a new Cairo::ImageSurface that is trimmed from the original
+    def compute_dimensions(sheet, batch)
+      w,h = batch.rotate ? [@height,@width] : [@width,@height]
+      sheet_width = (sheet.columns * (w + 2 * sheet.gap - 2 * sheet.trim)) + (2 * sheet.margin)
+      sheet_height = (sheet.rows * (h + 2 * sheet.gap - 2 * sheet.trim)) + (2 * sheet.margin)
+      return [w, h, sheet_width, sheet_height]
+    end
+
+    # Return a new Cairo::ImageSurface that is trimmed and rotated
+    # from the original
     #
     # @param surface The surface to trim
     # @param trim The number of pixels around the edge to trim
@@ -46,10 +56,15 @@ module Squib
     # @param height The height of the surface prior to the trim
     # :nodoc:
     # @api private
-    def trim(surface, trim, width, height)
-      if trim > 0
-        tmp = Cairo::ImageSurface.new(width - 2 * trim, height - 2 * trim)
+    def preprocess(surface, trim, w, h, rotate, angle)
+      if trim > 0 || rotate
+        tmp = Cairo::ImageSurface.new(w - 2 * trim, h - 2 * trim)
         cc = Cairo::Context.new(tmp)
+        if rotate
+            cc.translate w * 0.5, h * 0.5
+            cc.rotate angle
+            cc.translate h * -0.5, w * -0.5
+          end
         cc.set_source(surface, -1 * trim, -1 * trim)
         cc.paint
         surface = tmp
