55796a72c16ee033a1935259ff566955516e9d91
Andy Meneely <andy@se.rit.edu>
2021-10-22 00:49:50 -0400

    Feature done! Maybe?

--- lib/squib/graphics/save_doc.rb
+++ lib/squib/graphics/save_doc.rb
@@ -9,7 +9,7 @@ module Squib
       num_this_sheet = 0
       sheet_num = 0
       y = sheet.margin
-      x = sheet.rtl ? (sheet_width - sheet.margin - sheet.gap - w) : sheet.margin
+      x = sheet.rtl ? rtl_start_x(sheet_width, sheet, w) : sheet.margin
       @progress_bar.start("Saving PNG sheet to #{batch.summary}", @cards.size + 1) do |bar|
         range.each do |i|
           if num_this_sheet >= (sheet.columns * sheet.rows) # new sheet
@@ -19,7 +19,7 @@ module Squib
             num_this_sheet = 0
             sheet_num += 1
             y = sheet.margin
-            x = sheet.rtl ? (sheet_width - sheet.margin - sheet.gap - w) : sheet.margin
+            x = sheet.rtl ? rtl_start_x(sheet_width, sheet, w) : sheet.margin
             cc = Cairo::Context.new(Cairo::ImageSurface.new(sheet_width, sheet_height))
           end
           surface = preprocess(@cards[i].cairo_surface,
@@ -28,9 +28,9 @@ module Squib
           cc.set_source(surface, x, y)
           cc.paint
           num_this_sheet += 1
-          x += (surface.width + sheet.gap) * (sheet.rtl ? -1 : 1)
+          x += (w + sheet.gap - 2 * sheet.trim) * (sheet.rtl ? -1 : 1)
           if num_this_sheet % sheet.columns == 0 # new row
-            x = sheet.rtl ? (sheet_width - sheet.margin - sheet.gap - w) : sheet.margin
+            x = sheet.rtl ? rtl_start_x(sheet_width, sheet, w) : sheet.margin
             y += surface.height + sheet.gap
           end
 
@@ -47,23 +47,24 @@ module Squib
       return [w, h, sheet_width, sheet_height]
     end
 
+    def rtl_start_x(sheet_width, sheet, w)
+      return sheet_width - sheet.margin - sheet.gap - w + 2 * sheet.trim
+    end
+
     # Return a new Cairo::ImageSurface that is trimmed and rotated
     # from the original
-    #
-    # @param surface The surface to trim
-    # @param trim The number of pixels around the edge to trim
-    # @param width The width of the surface prior to the trim
-    # @param height The height of the surface prior to the trim
     # :nodoc:
     # @api private
     def preprocess(surface, trim, w, h, rotate, angle)
+      trimmed_w = w - 2 * trim
+      trimmed_h = h - 2 * trim
       if trim > 0 || rotate
-        tmp = Cairo::ImageSurface.new(w - 2 * trim, h - 2 * trim)
+        tmp = Cairo::ImageSurface.new(trimmed_w, trimmed_h)
         cc = Cairo::Context.new(tmp)
         if rotate
-            cc.translate w * 0.5, h * 0.5
+            cc.translate trimmed_w * 0.5, trimmed_h * 0.5
             cc.rotate angle
-            cc.translate h * -0.5, w * -0.5
+            cc.translate trimmed_h * -0.5, trimmed_w * -0.5
           end
         cc.set_source(surface, -1 * trim, -1 * trim)
         cc.paint
--- samples/saves/_saves.rb
+++ samples/saves/_saves.rb
@@ -3,69 +3,72 @@ require_relative '../../lib/squib'
 
 # This sample demonstrates how to use the various save methods
 
-# Squib::Deck.new(width: 825, height: 1125, cards: 16) do
-#   background color: :gray
-#   rect x: 38, y: 38, width: 750, height: 1050,
-#        x_radius: 38, y_radius: 38, stroke_width: 2.0, dash: '4 4'
+Squib::Deck.new(width: 825, height: 1125, cards: 16) do
+  background color: :gray
+  rect x: 38, y: 38, width: 750, height: 1050,
+       x_radius: 38, y_radius: 38, stroke_width: 2.0, dash: '4 4'
 
-#   text str: (1..16).to_a, x: 220, y: 78, font: 'Arial 18'
+  text str: (1..16).to_a, x: 220, y: 78, font: 'Arial 18'
 
-#   # Here's what a regular save_png looks like for just the first card
-#   save_png range: 0, prefix: 'save_png_'
+  # Here's what a regular save_png looks like for just the first card
+  save_png range: 0, prefix: 'save_png_'
 
-#   # save_png supports trim and trim_radius
-#   save_png trim: 30, trim_radius: 38,
-#            range: 0, prefix: 'save_png_trimmed_'
+  # save_png supports trim and trim_radius
+  save_png trim: 30, trim_radius: 38,
+           range: 0, prefix: 'save_png_trimmed_'
 
-#   # Place on multiple pages over the PDF, with bleed beeing trimmed off
-#   save_pdf file: 'save-pdf.pdf', margin: 75, gap: 5, trim: 37
+  # Place on multiple pages over the PDF, with bleed beeing trimmed off
+  save_pdf file: 'save-pdf.pdf', margin: 75, gap: 5, trim: 37
 
-#   # PDFs also support arbitrary paper sizes, in pixels or any other supported units
-#   save_pdf file: 'save-pdf-small.pdf',
-#            width: '7in', height: '5in',
-#            range: 0..1
+  # PDFs also support arbitrary paper sizes, in pixels or any other supported units
+  save_pdf file: 'save-pdf-small.pdf',
+           width: '7in', height: '5in',
+           range: 0..1
 
-#   # Note that our PNGs still are not trimmed even though the pdf ones were
-#   save_png range: 1, prefix: 'saves_notrim_'
+  # Note that our PNGs still are not trimmed even though the pdf ones were
+  save_png range: 1, prefix: 'saves_notrim_'
 
-#   # We can also save our PNGs into a single sheet,
-#   #  rows are calculated based on cols and number of cards
-#   save_sheet prefix: 'save_single_sheet_',
-#              columns: 2, margin: 75, gap: 5, trim: 37
+  # We can also save our PNGs into a single sheet,
+  #  rows are calculated based on cols and number of cards
+  save_sheet prefix: 'save_single_sheet_',
+             columns: 2, margin: 75, gap: 5, trim: 37
 
-#   # Or multiple sheets if rows are also specified
-#   save_sheet prefix: 'save_sheet_',
-#              columns: 4, rows: 2,
-#              margin: 75, gap: 5, trim: 37
+  # Or multiple sheets if rows are also specified
+  save_sheet prefix: 'save_sheet_',
+             columns: 4, rows: 2,
+             margin: 75, gap: 5, trim: 37
 
-#   # Sheets support ranges too
-#   save_sheet prefix: 'save_sheet_range_',
-#              range: 0..5,
-#              columns: 2, rows: 2,
-#              margin: 75, gap: 5, trim: 37
+  # Sheets support ranges too
+  save_sheet prefix: 'save_sheet_range_',
+             range: 0..5,
+             columns: 2, rows: 2,
+             margin: 75, gap: 5, trim: 37
 
-#   # Sheets can arrange left-to-right and right-to-left
-#   save_sheet prefix: 'save_sheet_rtl_',
-#              suffix: '_with_suffix',
-#              range: 0..1, rtl: true,
-#              columns: 2, rows: 1,
-#              margin: 75, gap: 5, trim: 37
-# end
+  # Sheets can arrange left-to-right and right-to-left
+  save_sheet prefix: 'save_sheet_rtl_',
+             suffix: '_with_suffix',
+             range: 0..1, rtl: true,
+             columns: 2, rows: 1,
+             margin: 75, gap: 5, trim: 37
+end
 
-# Squib::Deck.new(width: 100, height: 100, cards: 3) do
-#   background color: :grey
-#   text str: 'Hi', font: 'Arial 18'
+Squib::Deck.new(width: 100, height: 100, cards: 3) do
+  background color: :grey
+  text str: 'Hi', font: 'Arial 18'
 
-#   # Test bug 332.
-#   # When we only have 3 cards but want a 2x4 grid with lots of empty spaces.
-#   # Buggy behavior was to revert to 1 row and not respect the rows arg.
-#   save_sheet prefix: 'save_sheet_bug332_', rows: 2, columns: 4
-# end
+  # Test bug 332.
+  # When we only have 3 cards but want a 2x4 grid with lots of empty spaces.
+  # Buggy behavior was to revert to 1 row and not respect the rows arg.
+  save_sheet prefix: 'save_sheet_bug332_', rows: 2, columns: 4
+end
 
+# Allow rotating
 Squib::Deck.new(width: 100, height: 50, cards: 8) do
   background color: :white
   rect x: 10, y: 10, width: 80, height: 30
-  text str: 0..7, font: 'Open Sans Bold 8', align: :center, width: 100
+  rect x: 5, y: 5, width: 90, height: 40, stroke_width: 5, stroke_color: :blue
+  text y: 2, str: 0..7, font: 'Open Sans Bold 8', align: :center, width: 100
   save_sheet prefix: 'save_sheet_rotated_', rows: 2, columns: 3, rotate: true
   save_sheet prefix: 'save_sheet_rotated_trimmed_', rows: 2, columns: 3, rotate: true, trim: 5
+  save_sheet prefix: 'save_sheet_rotated_trimmed_rtl_', rows: 2, columns: 3, rotate: true, trim: 5, rtl: true
 end
